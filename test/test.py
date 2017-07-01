dd11e670fa27a1fd9c57a248a5678e0f8071e405
Soheil Hajian <soheil.hajian@outlook.com>
2017-07-01 15:29:45 +0200

    a test folder has been added

---
+++ test/test.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python2.7
+
+from src.glassdoor import GlassDoor
+
+import time
+
+Job = "Data Scientist"
+City = "Berlin"
+
+glassdoor = GlassDoor()
+glassdoor.search(Job,City)
+
+maxPage = 2
+
+for page in range(1,maxPage+1):
+    print "*** page "+ str(page) + " ***"
+    glassdoor.iterateMenu()
+    glassdoor.nextpage()
+
+
+
+# raw_input("Press a key")
+# glassdoor.browser.quit()
